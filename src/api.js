a799a743d2e9ee1c588587c7fc89b8a5ae684110
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-06-17 07:59:44 -0700

    de veldad toy jalto

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export const searchPokemons = async (pokemon) => {
 
     }
 };
-console.log('.')
+console.log('toy jalto')
 export const getPokemons = async (limit=0, offset=0) => {
     try {
         let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
