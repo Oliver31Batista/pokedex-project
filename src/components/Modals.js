4318f9feaafb50ab1490b82ce0c9778302dde05a
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-04 10:51:39 -0700

    logic applied to the customized modal hook

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useModal } from '../helpers/useModal'
 import Modal from './Modal'
 
 const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
   return (
     <div>
-        <h2>Modal</h2>
+        <h2>Modales</h2>
         <button>Modal 1</button>
         <Modal>
             <h3>Modal 1</h3>
@@ -12,11 +15,11 @@ const Modals = () => {
             <img src="https:placeimg.com/400/410/animals" alt='modal'/>
         </Modal>
         <button>Modal 2</button>
-        <Modal>
-            <h3>Otro modal</h3>
+        {/* <Modal>
+            <h3>Modal 1</h3>
             <p>ipsum dolor</p>
             <img src="https:placeimg.com/450/410/animals" alt='modal'/>
-        </Modal>
+        </Modal> */}
     </div>
   )
 }
