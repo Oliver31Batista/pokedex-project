3ee941568768b005da3774aeb7e6f9ab7c76c236
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-04 16:10:39 -0700

    modal is working

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,8 @@
 import "../css/Modal.css"
 
-const Modal = ({children}) => {
+const Modal = ({children, isOpen, closeModal}) => {
   return (
-    <article className="modal is-open">
+    <article className={`modal ${isOpen && "is-open"}`}>
         <div className="modal-container">
             <button className="modal-close">X</button>
             {children}
--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import { useModal } from '../helpers/useModal'
-import Modal from './Modal'
+import React from 'react';
+import { useModal } from '../hooks/useModal';
+import Modal from './Modal';
 
 const Modals = () => {
   const [isOpenModal1, openModal1, closeModal1] = useModal(false);
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+
   return (
-    <div>
+    <div>  
         <h2>Modales</h2>
-        <button>Modal 1</button>
-        <Modal>
+        <button onClick={openModal1}>Modal 1</button>
+        
+        <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
             <h3>Modal 1</h3>
             <p>Hola, este es el contenido de mi modal 1</p>
             <img src="https:placeimg.com/400/410/animals" alt='modal'/>
         </Modal>
-        <button>Modal 2</button>
-        {/* <Modal>
-            <h3>Modal 1</h3>
+        <button onClick={openModal2}>Modal 2</button>
+        <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+            <h3>Modal 2</h3>
             <p>ipsum dolor</p>
             <img src="https:placeimg.com/450/410/animals" alt='modal'/>
-        </Modal> */}
+        </Modal>
     </div>
   )
 }
