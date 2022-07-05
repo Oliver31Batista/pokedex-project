5c1c39a34d46d21b6a4ed01b42e70e27744c8509
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-04 23:13:21 -0400

    modal working perfectly

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,11 @@
 import "../css/Modal.css"
 
 const Modal = ({children, isOpen, closeModal}) => {
+  const handleModalContainerClick = e  => e.stopPropagation();
   return (
-    <article className={`modal ${isOpen && "is-open"}`}>
-        <div className="modal-container">
-            <button className="modal-close">X</button>
+    <article className={`modal ${isOpen && "is-open"}`} onClick={closeModal}>
+        <div className="modal-container" onClick={handleModalContainerClick}>
+            <button className="modal-close" onClick={closeModal}>X</button>
             {children}
         </div>
     </article>
